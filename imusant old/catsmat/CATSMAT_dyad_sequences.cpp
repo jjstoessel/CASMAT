ad64d6526e3c737458ed8c0611741188b24c9ce4
Derrick Hill <derrick.hill@TA055017MAC.local>
2015-07-11 08:50:38 +1000

    Reformatted CATSMAT_dyad_sequences.hpp and .cpp  for readability.  Added some debugging court's which are commented out. Whitespace changes only.

--- imusant old/catsmat/CATSMAT_dyad_sequences.cpp
+++ imusant old/catsmat/CATSMAT_dyad_sequences.cpp
@@ -15,153 +15,171 @@ using namespace ns_suffixtree;
 namespace CATSMAT
 {
     
-CATSMAT_dyad_sequences::CATSMAT_dyad_sequences(bool ignoreRepeatedDyads)
+    CATSMAT_dyad_sequences::CATSMAT_dyad_sequences(bool ignoreRepeatedDyads)
     : CATSMAT_dyad_sequences_base(ignoreRepeatedDyads)
-{
-    
-}
-    
-CATSMAT_dyad_sequences::~CATSMAT_dyad_sequences()
-{
-
-}
-
-void    CATSMAT_dyad_sequences::Visit(const CATSMAT_cp_matrix& matrix)
-{
-    //increase VectorInterval colection according to the formula of unique pairs n(n-1)/2
-    unsigned long partCount = matrix.partCount();
-    
-    while (fVIntervalVector.size() < ( (partCount)*(partCount-1)/2 ) )
     {
-        fVIntervalVector.push_back(new_IMUSANT_interval_vector());
-        fVIntervalVector.back()->setMaximum(1024);
+        
     }
-
-    process(matrix.getCPmatrix());
-}
-
-
-/*   
- \brief CATSMAT_dyad_sequences::process
     
-    Takes a list of chords (from CPMatrix)
-    1. calculates all vertical intervals between voices
-    
-*/
-    
-void    CATSMAT_dyad_sequences::process(const list<S_IMUSANT_chord>& matrix)
-{
-    if (!matrix.empty())
+    CATSMAT_dyad_sequences::~CATSMAT_dyad_sequences()
     {
-        for (auto chord = matrix.begin(); chord!=matrix.end(); chord++)
+        
+    }
+    
+    void
+    CATSMAT_dyad_sequences::Visit(const CATSMAT_cp_matrix& matrix)
+    {
+        //increase VectorInterval colection according to the formula of unique pairs n(n-1)/2
+        unsigned long partCount = matrix.partCount();
+        
+        while (fVIntervalVector.size() < ( (partCount)*(partCount-1)/2 ) )
         {
-            long i = 0;
-            
-            for (IMUSANT_vector<S_IMUSANT_note>::const_iterator note1 = (*chord)->getNotes().begin();
-                 note1 != (*chord)->getNotes().end();
-                 note1++)
+            fVIntervalVector.push_back(new_IMUSANT_interval_vector());
+            fVIntervalVector.back()->setMaximum(1024);
+        }
+        
+        process(matrix.getCPmatrix());
+    }
+    
+    
+    /*
+     \brief CATSMAT_dyad_sequences::process
+     
+     Takes a list of chords (from CPMatrix)
+     1. calculates all vertical intervals between voices
+     
+     */
+    
+    void
+    CATSMAT_dyad_sequences::process(const list<S_IMUSANT_chord>& matrix)
+    {
+        if (!matrix.empty())
+        {
+            for (auto chord = matrix.begin(); chord!=matrix.end(); chord++)
             {
-                for ( IMUSANT_vector<S_IMUSANT_note>::const_iterator note2 = note1;
-                     ++note2 != (*chord)->getNotes().end(); /*nothing here!*/)
+                long i = 0;
+                
+                for (IMUSANT_vector<S_IMUSANT_note>::const_iterator note1 = (*chord)->getNotes().begin();
+                     note1 != (*chord)->getNotes().end();
+                     note1++)
                 {
-                    //an interval is between two notes!
-                    if ((*note1)->getType()!=IMUSANT_NoteType::rest && (*note2)->getType()!=IMUSANT_NoteType::rest )
+                    for ( IMUSANT_vector<S_IMUSANT_note>::const_iterator note2 = note1;
+                         ++note2 != (*chord)->getNotes().end(); /*nothing here!*/)
                     {
-                        IMUSANT_interval interval((*note2)->pitch(), (*note1)->pitch());
+                        // cout << "Note 1: " << (*note1);
+                        // cout << "Note 2: " << (*note2);
                         
-                        interval.setLocation(i, (*note2)->getMeasureNum(), (*note2)->getNoteIndex(), (*note1)->getMeasureNum(), (*note1)->getNoteIndex());
-                        
-                        //only insert interval if not a repeated interval
-                        if (!((*note1)->isTiedPrevious() && (*note2)->isTiedPrevious()) && (interval.getQuality()!=IMUSANT_interval::dissonant))
+                        //an interval is between two notes!
+                        if ((*note1)->getType()!=IMUSANT_NoteType::rest && (*note2)->getType()!=IMUSANT_NoteType::rest )
                         {
-                            fVIntervalVector[i]->add(interval);
+                            IMUSANT_interval interval((*note2)->pitch(), (*note1)->pitch());
+                            
+                            // cout << "Interval: " << interval << std::endl;
+                            
+                            interval.setLocation(i,
+                                                 (*note2)->getMeasureNum(),
+                                                 (*note2)->getNoteIndex(),
+                                                 (*note1)->getMeasureNum(),
+                                                 (*note1)->getNoteIndex());
+                            
+                            //only insert interval if not a repeated interval
+                            if (!((*note1)->isTiedPrevious() && (*note2)->isTiedPrevious())
+                                &&
+                                (interval.getQuality()!=IMUSANT_interval::dissonant))
+                            {
+                                fVIntervalVector[i]->add(interval);
+                            }
                         }
+                        
+                        i++;
                     }
                     
-                    i++;
                 }
                 
+                fSaveI = i;
             }
-            
-            fSaveI = i;
         }
     }
-
-}
-
-
-void CATSMAT_dyad_sequences::find_repeated(int min)
-{
-    if (fVIntervalVector.size()>0)
+    
+    
+    void
+    CATSMAT_dyad_sequences::find_repeated(int min)
     {
-        for (auto i = fVIntervalVector.begin(); i!=fVIntervalVector.end(); i++)
+        if (fVIntervalVector.size()>0)
         {
-            interval_tree* tree = new interval_tree((*i)->getIntervals(),1); //change last parameter to id for whole work tree.
-            //cout << *tree;
-            vector<pair<interval_tree::size_type, interval_tree::size_type> >* results = new vector<pair<interval_tree::size_type, interval_tree::size_type> >();
-            find_repeated_substrings(*results, tree->root_node(), min);
-            
-            map<int, interval_tree::value_type > m = tree->get_sentences(); //const vector<IMUSANT_interval>& int_v = (*i)->getIntervals();
-            interval_tree::value_type int_v = m[1];
-            
-            cout << "duet: " << int_v[0].getLocation().partID << endl;
-            
-            for (auto j=results->begin(); j!=results->end(); j++)
+            for (auto i = fVIntervalVector.begin(); i!=fVIntervalVector.end(); i++)
             {
-                cout << "Sequence: (";
+                interval_tree* tree = new interval_tree((*i)->getIntervals(),1); //change last parameter to id for whole work tree.
+                //cout << *tree;
+                vector<pair<interval_tree::size_type, interval_tree::size_type> >* results = new vector<pair<interval_tree::size_type, interval_tree::size_type> >();
+                find_repeated_substrings(*results, tree->root_node(), min);
                 
-                cout << int_v[j->first].getLocation().first.measure;
-                cout << ", " << int_v[j->first].getLocation().first.note_index << ") ";
+                map<int, interval_tree::value_type > m = tree->get_sentences(); //const vector<IMUSANT_interval>& int_v = (*i)->getIntervals();
+                interval_tree::value_type int_v = m[1];
                 
-                for (auto k = j->first; k<j->second+j->first; k++)
+                cout << "duet: " << int_v[0].getLocation().partID << endl;
+                
+                for (auto j=results->begin(); j!=results->end(); j++)
                 {
-                    cout << int_v[k];
+                    cout << "Sequence: (";
+                    
+                    cout << int_v[j->first].getLocation().first.measure;
+                    cout << ", " << int_v[j->first].getLocation().first.note_index << ") ";
+                    
+                    for (auto k = j->first; k<j->second+j->first; k++)
+                    {
+                        cout << int_v[k];
+                    }
+                    cout << endl;
                 }
-                cout << endl;
+                
+                delete results;
+                delete tree;
             }
-            
-            delete results;
-            delete tree;
         }
     }
-}
-
-//private recursive function - needs to be templated
-void CATSMAT_dyad_sequences::find_repeated_substrings(vector< pair<interval_tree::size_type, interval_tree::size_type> >& results, const interval_tree::node& n, int min_length)
-{
-    interval_tree::size_type prefix_length = n.path_label_length() - (n.end() - n.begin());
     
-    if (n.children_size()==0)
+    //private recursive function - needs to be templated
+    void
+    CATSMAT_dyad_sequences::find_repeated_substrings(vector< pair<interval_tree::size_type,
+                                                     interval_tree::size_type> >& results,
+                                                     const interval_tree::node& n,
+                                                     int min_length)
     {
-        if (prefix_length>=min_length)
+        interval_tree::size_type prefix_length = n.path_label_length() - (n.end() - n.begin());
+        
+        if (n.children_size()==0)
         {
-            pair<interval_tree::size_type, interval_tree::size_type> res;
-            res.first=n.give_number().second; //give index of vector where branch starts
-            res.second=prefix_length;
-            results.push_back(res);
+            if (prefix_length>=min_length)
+            {
+                pair<interval_tree::size_type, interval_tree::size_type> res;
+                res.first=n.give_number().second; //give index of vector where branch starts
+                res.second=prefix_length;
+                results.push_back(res);
+            }
         }
-    }
-    
-    vector<interval_tree::element_type> v = n.next_children();
-    
-    if (!v.empty())
-    {
-        for (vector<interval_tree::element_type>::const_iterator it=v.begin();it!=v.end();it++)
+        
+        vector<interval_tree::element_type> v = n.next_children();
+        
+        if (!v.empty())
         {
-            const interval_tree::node* m = n.find_child(*it);
-            find_repeated_substrings(results, *m, min_length);
+            for (vector<interval_tree::element_type>::const_iterator it=v.begin();it!=v.end();it++)
+            {
+                const interval_tree::node* m = n.find_child(*it);
+                find_repeated_substrings(results, *m, min_length);
+            }
         }
     }
-}
     
-void    CATSMAT_dyad_sequences::print(ostream& os) const
-{
-    for (vector<S_IMUSANT_interval_vector>::const_iterator iter = fVIntervalVector.begin();
-         iter != fVIntervalVector.end() && (*iter) != NULL; iter++)
+    void
+    CATSMAT_dyad_sequences::print(ostream& os) const
     {
-        cout << "dyadic pair" << endl;
-        cout << *iter;
+        for (vector<S_IMUSANT_interval_vector>::const_iterator iter = fVIntervalVector.begin();
+             iter != fVIntervalVector.end() && (*iter) != NULL;
+             iter++)
+        {
+            os << "dyadic pair: " << *iter << endl;
+        }
     }
-}
-
-}//namespace CATSMAT
+    
+} //namespace CATSMAT
--- imusant old/catsmat/CATSMAT_dyad_sequences.hpp
+++ imusant old/catsmat/CATSMAT_dyad_sequences.hpp
@@ -21,30 +21,30 @@ using namespace ns_suffixtree;
 
 namespace CATSMAT
 {
-
-
-class CATSMAT_dyad_sequences: public CATSMAT_dyad_sequences_base
-{
-
-public:
-    typedef suffixtree< vector<IMUSANT_interval> > interval_tree;
+    class CATSMAT_dyad_sequences: public CATSMAT_dyad_sequences_base
+    {
+        
+    public:
+        typedef suffixtree< vector<IMUSANT_interval> > interval_tree;
+        
+        CATSMAT_dyad_sequences(bool ignoreRepeatedDyads=true);
+        ~CATSMAT_dyad_sequences();
+        
+        void    Visit(const CATSMAT_cp_matrix& matrix);
+        
+        void    find_repeated(int min=3);
+        void    print(ostream& os) const;
+        
+    private:
+        
+        void    process(const list<S_IMUSANT_chord>& matrix);
+        void    find_repeated_substrings(vector< pair<interval_tree::size_type, interval_tree::size_type> >& results,
+                                         const interval_tree::node& n,
+                                         int min_length=2);
+        
+        vector<S_IMUSANT_interval_vector>   fVIntervalVector;
+        long                                fSaveI = 0;
+    };
     
-    CATSMAT_dyad_sequences(bool ignoreRepeatedDyads=true);
-    ~CATSMAT_dyad_sequences();
-    
-    void    Visit(const CATSMAT_cp_matrix& matrix);
-    
-    void    find_repeated(int min=3);
-    void    print(ostream& os) const;
-    
-private:
-    
-    void    process(const list<S_IMUSANT_chord>& matrix);
-    void    find_repeated_substrings(vector< pair<interval_tree::size_type, interval_tree::size_type> >& results, const interval_tree::node& n, int min_length=2);
-    
-    vector<S_IMUSANT_interval_vector>   fVIntervalVector;
-    long                                fSaveI = 0;
-};
-
 } //namespace CATSMAT
 #endif /* defined(__imusant__CATSMAT_dyad_sequences__) */
