a2f962230b662a3856db6105167f3051b68acce7
Derrick Hill <derrick.hill@TA055017MAC.local>
2015-07-11 08:51:56 +1000

    Removed redundant #include from CATSMAT_Tests::main.cpp.

--- imusant old/CATSMAT_Tests/main.cpp
+++ imusant old/CATSMAT_Tests/main.cpp
@@ -7,10 +7,9 @@
 //
 
 #include <iostream>
-
 #include <gtest/gtest.h>
 
-#include "CATSMAT_cp_matrix.hpp"
+using namespace std;
 
 int main(int argc, char * argv[]) {
     
@@ -18,5 +17,5 @@ int main(int argc, char * argv[]) {
     cout << "---------------------" << endl;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
+    
 }
