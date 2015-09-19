58b8634a65ffcb4b2093bc1cc1293e230294d774
Derrick Hill <derrick.hill@TA055017MAC-2.local>
2015-09-19 22:26:04 +1000

    TK-01059 - Missed this file on last checkin. Fixing.

---
+++ imusant old/IMUSANT/Converters/MusicXML_v3/inc/IMUSANT_mxmlv3_to_imusant_visitor.h
@@ -0,0 +1,66 @@
+//
+//  IMUSANT_mxmlv3_to_imusant_visitor.h
+//  imusant
+//
+//  Created by Derrick Hill on 19/09/2015.
+//
+//  This class visits the nodes in a libMusicXML v3 score and translates
+//  this into an IMUSANT representation.
+//
+
+#ifndef imusant_IMUSANT_mxmlv3_to_imusant_visitor_h
+#define imusant_IMUSANT_mxmlv3_to_imusant_visitor_h
+
+#include "IMUSANT_score.h"
+
+#include "elements/typedefs.h"
+
+
+using namespace MusicXML2;
+
+namespace IMUSANT
+{
+    
+    class IMUSANT_mxmlv3_to_imusant_visitor
+    :
+    public visitor<S_score_partwise>,
+    public visitor<S_movement_title>,
+    public visitor<S_creator>,
+    public visitor<S_score_part>,
+    public visitor<S_part_name>,
+    public visitor<S_part>
+    {
+        
+    public:
+        
+        IMUSANT_mxmlv3_to_imusant_visitor();
+        
+        virtual ~IMUSANT_mxmlv3_to_imusant_visitor()
+        {
+        }
+        
+        S_IMUSANT_score get_imusant_score()
+        {
+            assert(fImusantScore!=0);
+            return fImusantScore;
+        }
+
+    protected:
+        
+        virtual void visitStart( S_score_partwise& elt);
+        virtual void visitStart( S_movement_title& elt);
+        virtual void visitStart( S_creator& elt);
+        virtual void visitStart( S_score_part& elt);
+        virtual void visitStart( S_part_name& elt);
+        virtual void visitStart( S_part& elt);
+        
+    private:
+        
+        S_IMUSANT_score fImusantScore;
+        
+    };
+}
+
+
+
+#endif
