5f4b1f0bc6e9cc06b2cda202d9be43c082ce955f
Derrick Hill <derrick@Water.local>
2015-10-22 02:05:16 +1000

    D-01012 - IMUSANT_key::xmlmode returns the wrong value for "minor" [https://www52.v1host.com/Private63/defect.mvc/Summary?oidToken=Defect%3A1275] This new test reproduces the problem reliably.

--- imusant old/IMUSANT_Tests/DomainObjects/IMUSANT_key_Tests.cpp
+++ imusant old/IMUSANT_Tests/DomainObjects/IMUSANT_key_Tests.cpp
@@ -31,7 +31,6 @@ protected:
 
 };
 
-
 TEST_F(IMUSANT_key_Tests, SmokeTest)
 {
     IMUSANT_key *the_key = new IMUSANT_key(3, IMUSANT_key::mixolydian, 0);
@@ -52,4 +51,53 @@ TEST_F(IMUSANT_key_Tests, SmokeTest)
     
     ASSERT_TRUE(the_key->isDefined());
 }
+
+TEST_F(IMUSANT_key_Tests, xmlmodeTest)
+{
+    // This test fails. BUG D-01012 raised in VersionOne.
+    IMUSANT_key *the_key = new IMUSANT_key(3, IMUSANT_key::mixolydian, 0);
+    
+    the_key->setMode(the_key->xmlmode("generic"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::generic);
+    
+    the_key->setMode(the_key->xmlmode("dorian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::dorian);
+    
+    the_key->setMode(the_key->xmlmode("hypodorian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::hypodorian);
+    
+    the_key->setMode(the_key->xmlmode("phrygian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::phrygian);
+    
+    the_key->setMode(the_key->xmlmode("hypophrygian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::hypophrygian);
+    
+    the_key->setMode(the_key->xmlmode("lydian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::lydian);
+    
+    the_key->setMode(the_key->xmlmode("hypolydian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::hypolydian);
+    
+    the_key->setMode(the_key->xmlmode("mixolydian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::mixolydian);
+    
+    the_key->setMode(the_key->xmlmode("hypomixolydian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::hypomixolydian);
+    
+    the_key->setMode(the_key->xmlmode("aeolian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::aeolian);
+    
+    the_key->setMode(the_key->xmlmode("locrian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::locrian);
+    
+    the_key->setMode(the_key->xmlmode("ionian"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::ionian);
+    
+    the_key->setMode(the_key->xmlmode("major"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::major);
+    
+    the_key->setMode(the_key->xmlmode("minor"));
+    ASSERT_TRUE(the_key->getMode() == IMUSANT_key::minor) << "This test fails. BUG D-01012 raised in VersionOne";
+}
+
 
