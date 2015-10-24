3cb8ba0ff2cd9a66bd7c43e6dc49a3fb1bb26f57
Derrick Hill <derrick@Water.local>
2015-10-24 16:05:44 +1000

    TK-01078 - Add a hand crafted test to exercise complex paths for the parser. [https://www52.v1host.com/Private63/Task.mvc/Summary?oidToken=Task%3A1284] - Added test file called MusicXML_ParserTest1.xml - Added test case into IMUSNAT_processing_Test

--- imusant old/IMUSANT_Tests/Analysis/IMUSANT_processing_Tests.cpp
+++ imusant old/IMUSANT_Tests/Analysis/IMUSANT_processing_Tests.cpp
@@ -182,7 +182,7 @@ TEST_F(IMUSANT_processing_Tests, FindRepeatedIntervalSubstrings_Dichterliebe01_m
     ASSERT_EQ(FindRepeatedIntervalSubstrings_Dichterliebe01_Expected, actual_output.str());
 }
 
-TEST_F(IMUSANT_processing_Tests, MXMLv3_Parser_CreatorTests)
+TEST_F(IMUSANT_processing_Tests, MXMLv3_Dichterliebe01)
 {
     string relative_path = "MusicXMLv3.xmlsamples/Dichterliebe01.xml";
     
@@ -252,6 +252,92 @@ TEST_F(IMUSANT_processing_Tests, MXMLv3_Parser_CreatorTests)
     IMUSANT_time p1_time = first_p1_measure->getTime();
     ASSERT_EQ(p1_time.getNumerator()[0], 2);
     ASSERT_EQ(p1_time.getDenominator()[0], 4);
+}
+
+TEST_F(IMUSANT_processing_Tests, MXMLv3_MusicXML_ParserTest1)
+{
+    string relative_path = "MusicXMLv3.simple_test_data/MusicXML_ParserTest1.xml";
+    
+    filesystem::path file_path = make_path_to_test_file(relative_path);
+    
+    IMUSANT_processing parser;
+    S_IMUSANT_score score = parser.process_musicxml3_file(file_path);
+    
+    /*
+     <movement-number>1</movement-number>
+     <movement-title>Im wunderschönen Monat Mai</movement-title>
+     */
+    ASSERT_EQ(score->getMovementNum(), "1");
+    ASSERT_EQ(score->getMovementTitle(), "Introduction");
+    
+    /*
+     <creator type="composer">Robert Schumann</creator>
+     <creator type="lyricist">Heinrich Heine</creator>
+     */
+    STRPAIRVECTOR creators = score->getCreator();
+    ASSERT_EQ(2, creators.size());
+    ASSERT_EQ(creators[0].first, "composer");
+    ASSERT_EQ(creators[0].second, "Derrick Hill");
+    ASSERT_EQ(creators[1].first, "lyricist");
+    ASSERT_EQ(creators[1].second, "Derrick Hill");
+    
+    
+    /*
+     <score-part id="P1">
+     ...
+     <score-part id="P2">
+     */
+    IMUSANT_vector<S_IMUSANT_part> part_list = score->partlist()->parts();
+    ASSERT_EQ(4, part_list.size());
+    ASSERT_EQ(part_list[0]->getPartName(), "Soprano");
+    ASSERT_EQ(part_list[1]->getPartName(), "Alto");
+    ASSERT_EQ(part_list[2]->getPartName(), "Tenor");
+    ASSERT_EQ(part_list[3]->getPartName(), "Bass");
+    
+    const int NUM_MEASURES = 42;
+    
+    IMUSANT_vector<S_IMUSANT_measure> pt1_measures = part_list[0]->measures();
+    ASSERT_EQ(NUM_MEASURES, pt1_measures.size());
+    IMUSANT_vector<S_IMUSANT_measure> pt2_measures = part_list[1]->measures();
+    ASSERT_EQ(NUM_MEASURES, pt2_measures.size());
+    
+    /*
+     part
+     */
+    S_IMUSANT_part p1;
+    S_IMUSANT_part p2;
+    
+    score->getPartById("P1", p1);
+    score->getPartById("P2", p2);
+    
+    IMUSANT_vector<S_IMUSANT_measure> p1_measures = p1->measures();
+    IMUSANT_vector<S_IMUSANT_measure> p2_measures = p2->measures();
+    
+    ASSERT_EQ(NUM_MEASURES, p1_measures.size());
+    ASSERT_EQ(NUM_MEASURES, p2_measures.size());
+    
+    S_IMUSANT_measure first_p1_measure = p1_measures[0];
+    
+    IMUSANT_key::mode p1_mode = first_p1_measure->getKey().getMode();
+    long p1_fifths = first_p1_measure->getKey().getFifths();
+    ASSERT_EQ(2, p1_fifths);
+    ASSERT_EQ(IMUSANT_key::mode::generic, p1_mode);
+    
+    IMUSANT_time p1_time = first_p1_measure->getTime();
+    ASSERT_EQ(p1_time.getNumerator()[0], 4);
+    ASSERT_EQ(p1_time.getDenominator()[0], 4);
+    
+    S_IMUSANT_measure change_p1_measure = p1_measures[7];
+    
+    p1_mode = change_p1_measure->getKey().getMode();
+    p1_fifths = change_p1_measure->getKey().getFifths();
+    ASSERT_EQ(3, p1_fifths);
+    ASSERT_EQ(IMUSANT_key::mode::generic, p1_mode);
+    
+    p1_time = change_p1_measure->getTime();
+    ASSERT_EQ(p1_time.getNumerator()[0], 6);
+    ASSERT_EQ(p1_time.getDenominator()[0], 8);
+    
 }
 
 
