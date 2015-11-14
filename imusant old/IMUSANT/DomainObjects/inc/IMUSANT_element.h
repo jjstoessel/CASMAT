1b2c248c39798f71d6553381ed8d0074f72bbd5e
Derrick Hill <derrick.hill@TA055017MAC.local>
2015-11-14 16:44:50 +1000

    TK-01083 - "Resolve polymorphism issues with IMUSANT_element"

--- imusant old/IMUSANT/DomainObjects/inc/IMUSANT_element.h
+++ imusant old/IMUSANT/DomainObjects/inc/IMUSANT_element.h
@@ -4,17 +4,17 @@
  *	Project Supervisor: Dr Rex Eakins
  *
  *  Created by Jason Stoessel on 22/03/06.
- * 
+ *
  *  Copyright 2006 Jason Stoessel. All rights reserved.
  *
  Notes:
-		These are the internal data structures of IMUSANT
+ These are the internal data structures of IMUSANT
  Changes:
-		4 May 2006: - After consulation with supervisor, it was decided that the model should implement an extensible
-					hierarchical model for data containment.  Although some of the elements of MusicXML will be excluded
-					from the present model, this represents the more complex but ultimately powerful implementation.
-					- basic MusicXML parsing is being obtained from the Grame MusicXML Library via the TXML2IMUSANTVisitor class
-					although heirarchical container class is still in coding phase.
+ 4 May 2006: - After consulation with supervisor, it was decided that the model should implement an extensible
+ hierarchical model for data containment.  Although some of the elements of MusicXML will be excluded
+ from the present model, this represents the more complex but ultimately powerful implementation.
+ - basic MusicXML parsing is being obtained from the Grame MusicXML Library via the TXML2IMUSANTVisitor class
+ although heirarchical container class is still in coding phase.
  */
 
 #ifndef __IMUSANT_element__
@@ -34,33 +34,33 @@ using namespace std;
 
 namespace IMUSANT
 {
-
-class IMUSANT_visitor;
-class IMUSANT_element;
-
-//typedef SMARTP<IMUSANT_element> 	S_IMUSANT_element;
-
-VEXP ostream& operator<< (ostream& os, const SMARTP<IMUSANT_element>& elt);
-
-
-/*!
-\brief A generic IMUSANT element base clase.
-
-*/
-class VEXP IMUSANT_element : public smartable, public IMUSANT_visitable 
-{
-	public:
-
-		void print (ostream& os) const;
-
+    
+    class IMUSANT_visitor;
+    class IMUSANT_element;
+    
+    VEXP ostream& operator<< (ostream& os, const SMARTP<IMUSANT_element>& elt);
+    
+    
+    /*!
+     \brief A generic IMUSANT element base clase.
+     
+     */
+    class VEXP IMUSANT_element : public smartable, public IMUSANT_visitable
+    {
+    public:
+        
+        virtual void print (ostream& os) const { os << "IMUSANT_element::print()" << endl;};
+        
         virtual void accept(IMUSANT_visitor& visitor);
-
+        
     protected:
-		IMUSANT_element() {}
-		virtual ~IMUSANT_element() {}
-};
-typedef SMARTP<IMUSANT_element> S_IMUSANT_element;
-
+        
+        IMUSANT_element() {}
+        virtual ~IMUSANT_element() {}
+    };
+    
+    typedef SMARTP<IMUSANT_element> S_IMUSANT_element;
+    
 } //namespace IMUSANT
 /*! @} */
 
--- imusant old/IMUSANT/DomainObjects/src/IMUSANT_element.cpp
+++ imusant old/IMUSANT/DomainObjects/src/IMUSANT_element.cpp
@@ -8,54 +8,24 @@
  */
 
 #include "IMUSANT_element.h"
-#include "IMUSANT_note.h"
-#include "IMUSANT_chord.h"
-#include "IMUSANT_comment.h"
 
 namespace IMUSANT
 {
-//______________________________________________________________________________
+    //______________________________________________________________________________
 #pragma mark friend constructors
-//S_IMUSANT_element new_IMUSANT_element()	
-//		{ IMUSANT_element * o = new IMUSANT_element(); assert(o!=0); return o; }
-
-//______________________________________________________________________________
+    
+    //______________________________________________________________________________
 #pragma mark stream operators
-ostream& operator<< (ostream& os, const S_IMUSANT_element& elt)
-{
-    elt->print(os);
-    return os;
-}
-
-//______________________________________________________________________________
-void IMUSANT_element::accept (IMUSANT_visitor& visitor)
-{
-	S_IMUSANT_element ptr(this);
-	visitor.visit(ptr);
-}
-
-void IMUSANT_element::print(ostream& os) const
-{
-	if (dynamic_cast<const IMUSANT_note*>(this)) 
-	{
-		S_IMUSANT_note note;
-		note.cast((IMUSANT_note*)this);
-		note->print(os);
-	}
-	
-	if (dynamic_cast<const IMUSANT_chord*>(this)) 
-	{
-		S_IMUSANT_chord chord;
-		chord.cast((IMUSANT_chord*)this);
-		chord->print(os);
-	}
-	
-	if (dynamic_cast<const IMUSANT_comment*>(this))
-	{
-		S_IMUSANT_comment comment;
-		comment.cast((IMUSANT_comment*)this);
-		comment->print(os);
-	}
-}
-
+    ostream& operator<< (ostream& os, const S_IMUSANT_element& elt)
+    {
+        elt->print(os);
+        return os;
+    }
+    
+    //______________________________________________________________________________
+    void IMUSANT_element::accept (IMUSANT_visitor& visitor)
+    {
+        S_IMUSANT_element ptr(this);
+        visitor.visit(ptr);
+    }
 }
