ed4b356ee0852fa3f92e032e2d3e90fa4bc40827
Derrick Hill <derrick@Water.local>
2015-11-21 13:23:09 +1000

    IMUSANT_barline.{cpp,h} - Whitespace changes only.

--- imusant old/IMUSANT/DomainObjects/inc/IMUSANT_barline.h
+++ imusant old/IMUSANT/DomainObjects/inc/IMUSANT_barline.h
@@ -17,128 +17,136 @@
 
 namespace IMUSANT
 {
-
-class IMUSANT_ending : public smartable
-{
-	public:
-		friend SMARTP<IMUSANT_ending> new_IMUSANT_ending();
-		
-		enum type { undefined=0, start=1, stop, last=stop };
-		static const string	xml (type d);
-		//! convert a string to a numeric value
-		static       type	xml (const string str);
-		//convert type to IMUSANT syllabic type
-		
-		IMUSANT_ending() : fType(undefined), fOrdinal(0){}
-		virtual ~IMUSANT_ending(){}
-		
-		void	setType(type t) { fType = t; }
-		void	setNumber(long times) { fOrdinal = times; }
-		type	getType() const { return fType; }
-		long	getNumber() const { return fOrdinal; }
-	
-	private:
-		type	fType;
-		long	fOrdinal;
-		
-		static bimap<string, type>	fType2String;
-		static type	fTypeTbl[];
-		static string	fTypeStrings[];
-};
-typedef SMARTP<IMUSANT_ending> S_IMUSANT_ending;
-    
-class IMUSANT_repeat : public smartable
-{
-	public:
-		friend SMARTP<IMUSANT_repeat> new_IMUSANT_repeat();
-		
-		enum direction { undefined=0, forward=1, backward, last=backward };
-		//convert type to string
-		static const string	xml (direction d);
-		//! convert a string to a numeric value
-		static       direction	xml (const string str);
-		
-		IMUSANT_repeat() : fDirection(undefined), fTimes(0) {}
-		virtual ~IMUSANT_repeat() {}
-		
-		void		setDirection(direction d) { fDirection = d; }
-		void		setTimes(long times) { fTimes=times; }
-		direction	getDirection() const { return fDirection; }
-		long		getTimes() const { return fTimes; }
-		
-	private:
-		direction	fDirection;
-		long		fTimes;
-		//could add laughing, humming, etc.
-		static bimap<string, direction>	fDirection2String;
-		static direction	fDirectionTbl[];
-		static string	fDirectionStrings[];
-};
-typedef SMARTP<IMUSANT_repeat> S_IMUSANT_repeat;
-
-class EXP IMUSANT_barline : public IMUSANT_element 
-{
+    //
+    //  IMUSANT_ending
+    //
+    class IMUSANT_ending : public smartable
+    {
     public:
-        enum location { undefined=0, right=1, left, middle, lastloc= middle };
-        enum type { none=1, regular, dotted, heavy, light_light, 
-                light_heavy, heavy_light, heavy_heavy, last=heavy_heavy };
- 
+        
+        friend SMARTP<IMUSANT_ending> new_IMUSANT_ending();
+        
+        enum type { undefined=0, start=1, stop, last=stop };
+        static const string	xml (type d);               // convert a string to a numeric value
+        static       type	xml (const string str);     // convert type to IMUSANT syllabic type
+        
+        IMUSANT_ending() : fType(undefined), fOrdinal(0){}
+        virtual ~IMUSANT_ending(){}
+        
+        void	setType(type t) { fType = t; }
+        void	setNumber(long times) { fOrdinal = times; }
+        type	getType() const { return fType; }
+        long	getNumber() const { return fOrdinal; }
+        
+    private:
+        type	fType;
+        long	fOrdinal;
+        
+        static bimap<string, type>	fType2String;
+        static type	fTypeTbl[];
+        static string	fTypeStrings[];
+    };
+    typedef SMARTP<IMUSANT_ending> S_IMUSANT_ending;
+    
+    
+    //
+    // IMUSANT_repeat
+    //
+    class IMUSANT_repeat : public smartable
+    {
+    public:
+        friend SMARTP<IMUSANT_repeat> new_IMUSANT_repeat();
+        
+        enum direction { undefined=0, forward=1, backward, last=backward };
+        static const string	xml (direction d);                 // convert type to string
+        static       direction	xml (const string str);        // convert a string to a numeric value
+        
+        IMUSANT_repeat() : fDirection(undefined), fTimes(0) {}
+        virtual ~IMUSANT_repeat() {}
+        
+        void		setDirection(direction d) { fDirection = d; }
+        void		setTimes(long times) { fTimes=times; }
+        direction	getDirection() const { return fDirection; }
+        long		getTimes() const { return fTimes; }
+        
+    private:
+        direction	fDirection;
+        long		fTimes;
+        //could add laughing, humming, etc.
+        static bimap<string, direction>	fDirection2String;
+        static direction	fDirectionTbl[];
+        static string	fDirectionStrings[];
+    };
+    typedef SMARTP<IMUSANT_repeat> S_IMUSANT_repeat;
+    
+    
+    //
+    // IMUSANT_barline
+    //
+    class EXP IMUSANT_barline : public IMUSANT_element
+    {
+    public:
+        enum location {
+            undefined=0, right=1, left, middle,
+            lastloc= middle
+        };
+        
+        enum type {
+            none=1, regular, dotted, heavy, light_light,
+            light_heavy, heavy_light, heavy_heavy,
+            last=heavy_heavy };
+        
         EXP friend SMARTP<IMUSANT_barline> new_IMUSANT_barline();
- 
-		virtual void accept(IMUSANT_visitor& visitor);
-
+        
+        virtual void accept(IMUSANT_visitor& visitor);
+        
         void		setLocation(location loc);
         void		setBarStyle(type style);
-		void		setEnding(const S_IMUSANT_ending ending) { fEnding = ending; }
-		void		setRepeat(const S_IMUSANT_repeat repeat) { fRepeat = repeat; }
-		
+        void		setEnding(const S_IMUSANT_ending ending) { fEnding = ending; }
+        void		setRepeat(const S_IMUSANT_repeat repeat) { fRepeat = repeat; }
+        
         location	getLocation()	const 	{ return fLocation; }
         type		getBarStyle()	const 	{ return fBarStyle; }
 
-        //SSegno&		segno()		 	{ return fSegno; }
-        //SCoda&		coda()		 	{ return fCoda; }
         S_IMUSANT_ending&	ending()		{ return fEnding; }
         S_IMUSANT_repeat&	repeat()		{ return fRepeat; }
-       
-	//! convert an integer location to a string
-	static const string	xmllocation (location d);
-	//! convert an string to an integer location
-	static location			xmllocation (const string str);
-	//! convert an integer bar style to a string
-	static const string	xmlstyle (type d);
-	//! convert an string to an integer bar style
-	static type			xmlstyle (const string str);
-
+        //SSegno&		segno()		 	{ return fSegno; }
+        //SCoda&		coda()		 	{ return fCoda; }
+        
+        static const string	xmllocation (location d);               // convert an integer location to a string
+        static location		xmllocation (const string str);         // convert an string to an integer location
+        
+        static const string	xmlstyle (type d);                      // convert an integer bar style to a string
+        static type			xmlstyle (const string str);            // convert an string to an integer bar style
+        
     protected:
         IMUSANT_barline() : fLocation(undefined), fBarStyle(none) {}
         virtual ~IMUSANT_barline() {}
-
+        
     private:
-        //! the barline location attribute (default to right)
-        location 		fLocation;
-        //! optionnal barline style
-        type			fBarStyle;
-	
-        //SSegno 		fSegno;
-        //SCoda 		fCoda;
+        
+        location 		fLocation;          // the barline location attribute (default to right)
+        type			fBarStyle;          // optionnal barline style
+        
         S_IMUSANT_ending	fEnding;
         S_IMUSANT_repeat	fRepeat;
-
-
-	static bimap<string, location> fLocation2String;
-	static location		fLocationTbl[];
-	static string		fLocationStrings[];
-
-	static bimap<string, type> fStyle2String;
-	static type 	fStyleTbl[];
-	static string 	fStyleStrings[];
-};
-typedef SMARTP<IMUSANT_barline> S_IMUSANT_barline;
-
+        //SSegno 		fSegno;
+        //SCoda 		fCoda;
+        
+        static bimap<string, location> fLocation2String;
+        static location		fLocationTbl[];
+        static string		fLocationStrings[];
+        
+        static bimap<string, type> fStyle2String;
+        static type 	fStyleTbl[];
+        static string 	fStyleStrings[];
+    };
+    typedef SMARTP<IMUSANT_barline> S_IMUSANT_barline;
+    
     SMARTP<IMUSANT_ending> new_IMUSANT_ending();
     SMARTP<IMUSANT_repeat> new_IMUSANT_repeat();
-EXP SMARTP<IMUSANT_barline> new_IMUSANT_barline();
-
+    EXP SMARTP<IMUSANT_barline> new_IMUSANT_barline();
+    
 } //namespace IMUSANT
 
 #endif //__IMUSANT_BARLINE
--- imusant old/IMUSANT/DomainObjects/src/IMUSANT_barline.cpp
+++ imusant old/IMUSANT/DomainObjects/src/IMUSANT_barline.cpp
@@ -10,65 +10,154 @@
 #include "IMUSANT_barline.h"
 namespace IMUSANT
 {
-//ctor functions
-S_IMUSANT_ending new_IMUSANT_ending()
-		{ IMUSANT_ending* o = new IMUSANT_ending(); assert (o!=0); return o; }
-S_IMUSANT_repeat new_IMUSANT_repeat()
-		{ IMUSANT_repeat* o = new IMUSANT_repeat(); assert (o!=0); return o; }
-S_IMUSANT_barline new_IMUSANT_barline() 
-		{ IMUSANT_barline* o = new IMUSANT_barline(); assert(o!=0); return o; }
-
-//===========================================================================+
-//IMUSANT_ending
-					
-IMUSANT_ending::type	IMUSANT_ending::fTypeTbl[] = { undefined, start, stop };						
-string	IMUSANT_ending::fTypeStrings[] = { "undefined", "start", "stop" };
-bimap<string, IMUSANT_ending::type>	IMUSANT_ending::fType2String(fTypeStrings, fTypeTbl, last);
-
+    //ctor functions
+    S_IMUSANT_ending new_IMUSANT_ending()
+    { IMUSANT_ending* o = new IMUSANT_ending(); assert (o!=0); return o; }
+    
+    S_IMUSANT_repeat new_IMUSANT_repeat()
+    { IMUSANT_repeat* o = new IMUSANT_repeat(); assert (o!=0); return o; }
+    
+    S_IMUSANT_barline new_IMUSANT_barline()
+    { IMUSANT_barline* o = new IMUSANT_barline(); assert(o!=0); return o; }
+    
+    //===========================================================================+
+    //IMUSANT_ending
+    
+    IMUSANT_ending::type IMUSANT_ending::fTypeTbl[] =
+    {
+        undefined, start, stop
+    };
+    
+    string	IMUSANT_ending::fTypeStrings[] =
+    {
+        "undefined", "start", "stop"
+    };
+    
+    bimap<string, IMUSANT_ending::type>	IMUSANT_ending::fType2String(fTypeStrings, fTypeTbl, last);
+    
     //! convert a numeric start-stop value to a MusicXML string
-const string IMUSANT_ending::xml (IMUSANT_ending::type d) { return fType2String[d]; }
-
+    const string
+    IMUSANT_ending::
+    xml (IMUSANT_ending::type d)
+    {
+        return fType2String[d];
+    }
+    
     //! convert a MusicXML string to a numeric value
-IMUSANT_ending::type	IMUSANT_ending::xml (const string str) { return fType2String[str]; }
-
-//==============================================================================
-//IMUSANT_repeat
-					
-IMUSANT_repeat::direction	IMUSANT_repeat::fDirectionTbl[] = { undefined, forward, backward };						
-string	IMUSANT_repeat::fDirectionStrings[] = { "undefined", "forward", "backward" };
-bimap<string, IMUSANT_repeat::direction>	IMUSANT_repeat::fDirection2String(fDirectionStrings, fDirectionTbl, last);
-//! convert a numeric start-stop value to a MusicXML string
-const string IMUSANT_repeat::xml (IMUSANT_repeat::direction d) { return fDirection2String[d]; }
-//! convert a MusicXML string to a numeric value
-IMUSANT_repeat::direction	IMUSANT_repeat::xml (const string str) { return fDirection2String[str]; }
-
-//==============================================================================
-//IMUSANT_barline
-
-IMUSANT_barline::location IMUSANT_barline::fLocationTbl[] 		= { right, left, middle };
-string IMUSANT_barline::fLocationStrings[]	= { "right", "left", "middle" };
-bimap<string, IMUSANT_barline::location> IMUSANT_barline::fLocation2String(fLocationStrings, fLocationTbl, lastloc);
-
-IMUSANT_barline::type IMUSANT_barline::fStyleTbl[] 			= { none, regular, dotted, heavy, light_light, 
-                                        light_heavy, heavy_light, heavy_heavy };
-string IMUSANT_barline::fStyleStrings[]	= { "none", "regular", "dotted", "heavy", "light-light", 
-                                        "light-heavy", "heavy-light", "heavy-heavy" };
-bimap<string, IMUSANT_barline::type> IMUSANT_barline::fStyle2String(fStyleStrings, fStyleTbl, last);
-
-//______________________________________________________________________________
-void IMUSANT_barline::setLocation(location loc)		{ fLocation = loc; }
-void IMUSANT_barline::setBarStyle(type style)	{ fBarStyle = style; }
-//______________________________________________________________________________
-const string IMUSANT_barline::xmllocation (location d) 		{ return fLocation2String[d]; }
-IMUSANT_barline::location IMUSANT_barline::xmllocation (const string str) 	{ return fLocation2String[str]; }
-const string IMUSANT_barline::xmlstyle (type d) 		{ return fStyle2String[d]; }
-IMUSANT_barline::type IMUSANT_barline::xmlstyle (const string str) 		{ return fStyle2String[str]; }
-
-//______________________________________________________________________________
-void IMUSANT_barline::accept(IMUSANT_visitor& visitor)
-{
-	S_IMUSANT_barline sptr(this);
-	visitor.visit(sptr);
-}
-
+    IMUSANT_ending::type
+    IMUSANT_ending::
+    xml (const string str)
+    {
+        return fType2String[str];
+    }
+    
+    //==============================================================================
+    //IMUSANT_repeat
+    
+    IMUSANT_repeat::direction IMUSANT_repeat::fDirectionTbl[] =
+    {
+        undefined, forward, backward
+    };
+    
+    string	IMUSANT_repeat::fDirectionStrings[] =
+    {
+        "undefined", "forward", "backward"
+    };
+    
+    bimap<string, IMUSANT_repeat::direction> IMUSANT_repeat::fDirection2String(fDirectionStrings, fDirectionTbl, last);
+    
+    //! convert a numeric start-stop value to a MusicXML string
+    const string
+    IMUSANT_repeat::
+    xml (IMUSANT_repeat::direction d)
+    {
+        return fDirection2String[d];
+    }
+    
+    //! convert a MusicXML string to a numeric value
+    IMUSANT_repeat::direction
+    IMUSANT_repeat::
+    xml (const string str)
+    {
+        return fDirection2String[str];
+    }
+    
+    //==============================================================================
+    //IMUSANT_barline
+    
+    IMUSANT_barline::location IMUSANT_barline::fLocationTbl[] =
+    {
+        right, left, middle
+    };
+    
+    string IMUSANT_barline::fLocationStrings[]	=
+    {
+        "right", "left", "middle"
+    };
+    
+    bimap<string, IMUSANT_barline::location> IMUSANT_barline::fLocation2String(fLocationStrings, fLocationTbl, lastloc);
+    
+    IMUSANT_barline::type IMUSANT_barline::fStyleTbl[] =
+    {
+        none, regular, dotted, heavy, light_light,
+        light_heavy, heavy_light, heavy_heavy
+    };
+    
+    string IMUSANT_barline::fStyleStrings[]	=
+    {
+        "none", "regular", "dotted", "heavy", "light-light",
+        "light-heavy", "heavy-light", "heavy-heavy"
+    };
+    
+    bimap<string, IMUSANT_barline::type> IMUSANT_barline::fStyle2String(fStyleStrings, fStyleTbl, last);
+    
+    void
+    IMUSANT_barline::
+    setLocation(location loc)
+    {
+        fLocation = loc;
+    }
+    
+    void
+    IMUSANT_barline::
+    setBarStyle(type style)
+    {
+        fBarStyle = style;
+    }
+    
+    
+    const string
+    IMUSANT_barline::
+    xmllocation(location d)
+    {
+        return fLocation2String[d];
+    }
+    
+    IMUSANT_barline::location
+    IMUSANT_barline::
+    xmllocation(const string str)
+    {
+        return fLocation2String[str];
+    }
+    
+    const string
+    IMUSANT_barline::xmlstyle(type d)
+    {
+        return fStyle2String[d];
+    }
+    
+    IMUSANT_barline::type
+    IMUSANT_barline::xmlstyle(const string str)
+    {
+        return fStyle2String[str];
+    }
+    
+    void
+    IMUSANT_barline::
+    accept(IMUSANT_visitor& visitor)
+    {
+        S_IMUSANT_barline sptr(this);
+        visitor.visit(sptr);
+    }
+    
 }//namespace IMUSANT
