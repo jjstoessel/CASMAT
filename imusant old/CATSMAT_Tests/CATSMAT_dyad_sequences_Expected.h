42d4f861372813b5d1f88f0fb4a69fd61d1649ce
jjstoessel <jason.stoessel@une.edu.au>
2016-03-09 15:22:36 +1100

    Defect D-01020 Updated expected CATSMAT_dyad_sequences_Expected.h and dCATSMAT_dyadtuple_sequences_Expected.h to match new procedure for handling octaves and outputting intervals. 16 CATSMAT tests passed

--- imusant old/CATSMAT_Tests/CATSMAT_dyad_sequences_Expected.h
+++ imusant old/CATSMAT_Tests/CATSMAT_dyad_sequences_Expected.h
@@ -9,13 +9,13 @@
 #ifndef imusant_CATSMAT_dyad_sequences_Expected_h
 #define imusant_CATSMAT_dyad_sequences_Expected_h
 
-const string TestScore_1_Measure_Expected = "dyadic pair: octave / , per5 / , maj3 / , octave / , \n\n";
+const string TestScore_1_Measure_Expected = "dyadic pair: per1  (1, 1), per5  (1, 2), maj3  (1, 3), per1  (1, 4), \n\n";
 
-const string TestScore_4_Measures_Expected = "dyadic pair: octave / , min6 / , octave / , maj3 / , maj6 / , octave / , maj3 / , per5 / , min3 / , octave / , maj3 / , octave / , \n\n";
+const string TestScore_4_Measures_Expected = "dyadic pair: per1  (1, 1), min6  (1, 3), per1  (1, 4), maj3  (2, 1), maj6  (2, 2), per1  (2, 3), maj3  (2, 4), per5  (3, 1), min3  (3, 2), per1  (3, 3), maj3  (3, 4), per1  (4, 1), \n\n";
 
 
-const string TestScore_4_Measures_WithQuaverPassingNotes_Expected = "dyadic pair: octave / , min6 / , min6 / , octave / , maj3 / , octave / , maj6 / , octave / , maj3 / , per5 / , min3 / , octave / , maj3 / , per5 / , octave / , \n\n";
+const string TestScore_4_Measures_WithQuaverPassingNotes_Expected = "dyadic pair: per1  (1, 1), min6  (1, 2), min6  (1, 4), per1  (1, 5), maj3  (2, 1), per1  (2, 2), maj6  (2, 3), per1  (2, 4), maj3  (2, 5), per5  (3, 1), min3  (3, 2), per1  (3, 3), maj3  (3, 4), per5  (3, 5), per1  (4, 1), \n\n";
 
-const string TestScore_4_Measures_WithSemiQuaverPassingNotes_Expected = "dyadic pair: octave / , min6 / , per5 / , min6 / , octave / , maj3 / , octave / , maj6 / , octave / , maj3 / , per5 / , min3 / , octave / , octave / , maj3 / , per5 / , maj6 / , octave / , \n\n";
+const string TestScore_4_Measures_WithSemiQuaverPassingNotes_Expected = "dyadic pair: per1  (1, 1), min6  (1, 3), per5  (1, 4), min6  (1, 6), per1  (1, 7), maj3  (2, 1), per1  (2, 2), maj6  (2, 3), per1  (2, 4), maj3  (2, 5), per5  (3, 1), min3  (3, 2), per1  (3, 3), per1  (3, 3), maj3  (3, 4), per5  (3, 6), maj6  (3, 7), per1  (4, 1), \n\n";
 
 #endif
