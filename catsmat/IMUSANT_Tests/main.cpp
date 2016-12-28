610b5d10340fe34f22a8e86ef90244b84f11a98b
Derrick Hill <derrick@derricks-imac.local>
2016-12-28 13:56:51 +1000

    Refined exclusion flags.  You can use "-" to exclude specific tests and run everything else.

--- catsmat/IMUSANT_Tests/main.cpp
+++ catsmat/IMUSANT_Tests/main.cpp
@@ -17,7 +17,14 @@ int main(int argc, char * argv[]) {
     
     cout << "RUNNING IMUSANT TESTS" << endl;
     cout << "---------------------" << endl;
- //   ::testing::GTEST_FLAG(filter) = "*GetSegmentsAsNoteVectors_From_Score_Kyrie*";   // Syntax for limiting which tests get run.
+    
+    // Exclude specific tests...
+    // ::testing::GTEST_FLAG(filter) = "-IMUSANT_segmented_part_fixed_period_Tests*" ;
+    
+    // Include specific tests...
+    // ::testing::GTEST_FLAG(filter) = "IMUSANT_partlist_ordered_by_part_entry_Tests*" ;
+
+ 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
     
