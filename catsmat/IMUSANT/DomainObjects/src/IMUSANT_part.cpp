56a5965126f229943dff7e945ac832be9a50fac4
Derrick Hill <derrick@Derricks-iMac.local>
2017-01-11 15:35:27 +1000

    IMUSANT_part - whitespace changes only.

--- catsmat/IMUSANT/DomainObjects/src/IMUSANT_part.cpp
+++ catsmat/IMUSANT/DomainObjects/src/IMUSANT_part.cpp
@@ -11,23 +11,23 @@
 
 namespace IMUSANT
 {
-
-S_IMUSANT_part new_IMUSANT_part() 
-		{ IMUSANT_part* o = new IMUSANT_part(); assert (o!=0); return o; }
-
-//______________________________________________________________________________
-IMUSANT_part::IMUSANT_part() 
-{
-}
-
-//------------------------------------------------------------------------------
-// Acceptor function
-//------------------------------------------------------------------------------
-void IMUSANT_part::accept(IMUSANT_visitor& visitor)
-{
-	S_IMUSANT_part sptr(this);
-	visitor.visit(sptr);
-}
+    
+    S_IMUSANT_part new_IMUSANT_part()
+    { IMUSANT_part* o = new IMUSANT_part(); assert (o!=0); return o; }
+    
+    //______________________________________________________________________________
+    IMUSANT_part::IMUSANT_part()
+    {
+    }
+    
+    //------------------------------------------------------------------------------
+    // Acceptor function
+    //------------------------------------------------------------------------------
+    void IMUSANT_part::accept(IMUSANT_visitor& visitor)
+    {
+        S_IMUSANT_part sptr(this);
+        visitor.visit(sptr);
+    }
     
     
     IMUSANT_vector<S_IMUSANT_note>
@@ -43,20 +43,20 @@ void IMUSANT_part::accept(IMUSANT_visitor& visitor)
         
         return the_notes;
     }
-
-void IMUSANT_part::print(ostream& os)
-{
-	os << "<PART>" << endl;
-	os << "Part name: " << fPartName << endl;
-	os << "Part abbrevavtion: " << fPartAbbrev << endl;
-	os << "Part Id: " << fXMLID << endl;
-	
-	for (vector<S_IMUSANT_measure>::const_iterator measure=fMeasures.begin(); measure!=fMeasures.end(); measure++)
-	{
-		(*measure)->print(os);
-	}
-	
-	os << "<\\PART>" << endl;
-}
-
+    
+    void IMUSANT_part::print(ostream& os)
+    {
+        os << "<PART>" << endl;
+        os << "Part name: " << fPartName << endl;
+        os << "Part abbrevavtion: " << fPartAbbrev << endl;
+        os << "Part Id: " << fXMLID << endl;
+        
+        for (vector<S_IMUSANT_measure>::const_iterator measure=fMeasures.begin(); measure!=fMeasures.end(); measure++)
+        {
+            (*measure)->print(os);
+        }
+        
+        os << "<\\PART>" << endl;
+    }
+    
 } //namespace IMUSANT
