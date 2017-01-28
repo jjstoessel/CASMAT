3d5217e0fbdcc341d9b265d17b9bf34037cc5564
Derrick Hill <derrick@derricks-imac.local>
2017-01-28 11:53:29 +1000

    S-01217 - Segmentation - Implement periodic segmentation across an entire score TK-01338 - Fix the selection of segments to ensure that each part is segmented once and once only. FixedPeriodSegmentation_Initialise_Sanctus now passes.

--- catsmat/IMUSANT_Tests/Analysis/IMUSANT_segmented_part_fixed_period_Tests.cpp
+++ catsmat/IMUSANT_Tests/Analysis/IMUSANT_segmented_part_fixed_period_Tests.cpp
@@ -178,8 +178,8 @@ TEST_F(IMUSANT_segmented_part_fixed_period_Tests, FixedPeriodSegmentation_Initia
     //
     // A: By part:
     //   Ca1 has 8     (63 bars / 7 bar segments, but the last 7 bars are not a segment)
-    //   Ca2 has 8     (Ca1 - 1 because Ca2 starts one segment later)
-    //   T   has 7     (Ca1 - 2 because TY starts 2 segments later)
+    //   Ca2 has 8     (= Ca1  : because Ca2 starts one segment later than Ca1, but it's last segment is included)
+    //   T   has 7     (= Ca2 - 1 : because T starts 1 segment later than Ca2)
     //   -----------
     //   TOTAL  23
     
@@ -236,33 +236,41 @@ TEST_F(IMUSANT_segmented_part_fixed_period_Tests, FixedPeriodSegmentation_Initia
 
 TEST_F(IMUSANT_segmented_part_fixed_period_Tests, FixedPeriodSegmentation_Initialise_Sanctus)
 {
+    
+    // Q: How many segments are there in Sanctus?
+    //
+    // A: By part:
+    //   Ca1 has 22    (115 bars / 5 bar segments, but the last 7 bars are not a segment)
+    //   Ca2 has 22    (= Ca1 :  because Ca2 starts one segment later, but it's last segment is included)
+    //   T   has 21    (= Ca2 - 1 : because T starts 1 segments later than Ca2)
+    //   -----------
+    //   TOTAL   65
+    
+    
     S_IMUSANT_segmented_part_fixed_period s_segmented_part = new_IMUSANT_segmented_part_fixed_period();
-    int ret_val = s_segmented_part->initialise(fScore_Sanctus);
+    
+    const double ERROR_THRESHOLD = 0.2;
+    int ret_val = s_segmented_part->initialise(fScore_Sanctus, ERROR_THRESHOLD);
     
     ASSERT_EQ(IMUSANT_segmented_part_fixed_period::SUCCESS, ret_val);
     ASSERT_EQ(7680, s_segmented_part->getPeriodDurationForThisScore()->asAbsoluteNumeric() );
     
-//    vector<S_IMUSANT_segment> segments = s_segmented_part->getSegments();
-//    ASSERT_EQ(23, segments.size()) << "Unexpected number of segments...";
-    
     SetOfSegments segmentset = s_segmented_part->getSegmentsSet();
     
+#ifdef VERBOSE
     cout << endl << "----- SEGMENTS -----" << endl;
     for (SetOfSegments::iterator it = segmentset.begin(); it != segmentset.end(); it++ )
     {
         cout << *it << endl;
     }
+#endif
     
-    for (SetOfSegments::iterator it = segmentset.begin() ;
-         it != segmentset.end();
-         it++)
-    {
-        ASSERT_EQ(*fScore_Kyrie_TwoPartsOnly, *it->getScore());
-    }
+    ASSERT_EQ(65, segmentset.size()) << "Unexpected number of segments in set...";
     
-//    for (int seg_index = 0 ; seg_index < segments.size(); seg_index++)
-//    {
-//        ASSERT_EQ(*fScore_Sanctus, *(segments[seg_index]->getScore()));
-//    }
+    string FixedPeriodSegmentation_Initialise_Sanctus_Expected = "Sanctus:Ca 1:101:1:105:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:106:1:110:1:12:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:11:1:15:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:16:1:20:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:1:1:5:1:5:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:21:1:25:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:26:1:30:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:31:1:35:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:36:1:40:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:41:1:45:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:46:1:50:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:51:1:55:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:56:1:60:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:61:1:65:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:66:1:70:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:6:1:10:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:71:1:75:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:76:1:80:1:11:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:81:1:85:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:86:1:90:1:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:91:1:95:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 1:96:1:100:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:101:1:105:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:106:1:110:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:111:1:115:1:12:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:11:1:15:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:16:1:20:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:21:1:25:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:26:1:30:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:31:1:35:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:36:1:40:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:41:1:45:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:46:1:50:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:51:1:55:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:56:1:60:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:61:1:65:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:66:1:70:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:6:1:10:1:5:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:71:1:75:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:76:1:80:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:81:1:85:1:11:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:86:1:90:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:91:1:95:1:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:Ca 2:96:1:100:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:101:1:105:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:106:1:110:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:111:1:115:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:11:1:15:1:5:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:16:1:20:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:21:1:25:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:26:1:30:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:31:1:35:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:36:1:40:2:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:41:1:45:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:46:1:50:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:51:1:55:2:8:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:56:1:60:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:61:1:65:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:66:1:70:1:7:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:71:1:75:1:10:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:76:1:80:1:6:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:81:1:85:1:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:86:1:90:1:11:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:91:1:95:2:9:0:IMUSANT_segmented_part_fixed_period\nSanctus:T:96:1:100:1:8:0:IMUSANT_segmented_part_fixed_period\n\n";
+    
+    string actual_output = getActualSegmentsAsString(segmentset);
+    
+    ASSERT_EQ(FixedPeriodSegmentation_Initialise_Sanctus_Expected, actual_output);
 }
 
